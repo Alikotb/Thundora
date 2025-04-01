99728b777c015945ae70e9658692affa4968a8fa
Ali Kotb <AliKotb38@gmail.com>
2025-04-01 16:15:55 +0200

    test: Adda unit tests for inserting and deleting alarms in LocalDataSource
    - Implemented unit test for inserting an alarm and verifying database persistence.
    - Implemented unit test for deleting an alarm and ensuring it is removed from the database.
    - Used coroutines and Room database to validate expected behavior.
    - Fixed assertion issues related to ID retrieval after insertion.

--- app/src/androidTest/java/com/example/thundora/data/local/source/LocalDataSourceTest.kt
+++ app/src/androidTest/java/com/example/thundora/data/local/source/LocalDataSourceTest.kt
@@ -2,15 +2,24 @@ package com.example.thundora.data.local.source
 
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.thundora.data.local.database.Dao
 import com.example.thundora.data.local.database.WeatherDataBase
 import com.example.thundora.data.local.sharedpreference.SharedPreference
+import com.example.thundora.domain.model.api.AlarmEntity
 import io.mockk.mockk
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
+import java.time.LocalTime
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
@@ -38,5 +47,32 @@ class LocalDataSourceTest {
     @After
     fun tearDown() = database.close()
 
+
+
+    @Test
+    fun insertAlarm_StoresAlarmInDatabase() = runTest {
+        val alarm = AlarmEntity(0, "Alaram1", LocalTime.now(), 60)
+
+       localDataSource.insertAlarm(alarm)
+        val result = localDataSource.getAlarmById(alarm.id)
+
+        assertNotNull(result)
+        assertThat(result?.label, `is`("Alaram1"))
+        assertThat(result?.duration, `is`(60))
+        assertThat(result?.id, `is`(result?.id))
+    }
+
+
+
+    @Test
+    fun updateAlarm_UpdatesAlarmInDatabase() = runTest {
+        val alarm= AlarmEntity(0,"Alaram1",LocalTime.now(),60)
+        localDataSource.insertAlarm(alarm)
+        val updatedAlaram=alarm.copy(label = "updated")
+        localDataSource.updateAlarm(updatedAlaram)
+        val result = localDataSource.getAlarmById(alarm.id)
+        assertNotNull(result)
+        MatcherAssert.assertThat(result?.label, `is`("updated"))
+    }
 
 }
